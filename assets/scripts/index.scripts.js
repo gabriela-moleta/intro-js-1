636aeafadf8db9959f5ba85628e9f30e24d1e28e
Gabriela Moleta <gabriela.moleta@aluno.senai.br>
2025-02-04 08:54:14 -0300

    feat: show input text in tag p

--- assets/scripts/index.scripts.js
+++ assets/scripts/index.scripts.js
@@ -2,4 +2,7 @@ function exibirNome() {
     let nome = document.getElementById("nome").value;
 
     console.log("Nome digitado: ", nome);
+
+    document.getElementById("msg").innerHTML = nome;
 }
+
